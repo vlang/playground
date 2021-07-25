ecf2a286c0a2862888b651eec18fe70a3d94be01
shadowninja55 <shadowninja1050@gmail.com>
2021-07-25 14:12:05 -0400

    remove useless file

--- server.v
+++ server.v
@@ -91,7 +91,6 @@ fn (mut app App) init_once() {
 	os.execute('isolate --cleanup')
 	app.handle_static('static', true)
 	app.serve_static('/static/js/codejar.js', 'static/js/codejar.js')
-	app.serve_static('/static/js/linenumbers.js', 'static/js/linenumbers.js')
 }
 
 fn main() {
