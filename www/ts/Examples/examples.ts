af715ab31180fd62dbe54c7efa2481b2ffa46ed2
walkingdevel <mosquito.is.dead@gmail.com>
2023-01-08 17:21:31 +0000

    fix: format examples

--- www/ts/Examples/examples.ts
+++ www/ts/Examples/examples.ts
@@ -14,17 +14,19 @@ println('Hello, world!')
         name: "Fibonacci",
         code: `
 fn fib(n int) int {
-    mut f := []int{len: n + 2}
-    f[0] = 0
-    f[1] = 1
-    for i := 2; i <= n; i++ {
-        f[i] = f[i - 1] + f[i - 2]
-    }
-    return f[n]
+	mut f := []int{len: n + 2}
+	f[0] = 0
+	f[1] = 1
+
+	for i := 2; i <= n; i++ {
+		f[i] = f[i - 1] + f[i - 2]
+	}
+
+	return f[n]
 }
 
 for i in 0 .. 30 {
-    println(fib(i))
+	println(fib(i))
 }
 `,
     },
@@ -32,8 +34,9 @@ for i in 0 .. 30 {
         name: "String interpolation",
         code: `
 areas := ['game', 'web', 'tools', 'science', 'systems', 'embedded', 'drivers', 'GUI', 'mobile']
+
 for area in areas {
-    println('Hello, $area developers!')
+	println('Hello, \${area} developers!')
 }
 `,
     },
@@ -43,45 +46,45 @@ for area in areas {
 import json
 
 struct User {
-    name string
-    age  int
+	name string
+	age  int
 mut:
-    is_registered bool
+	is_registered bool
 }
 
 fn main() {
-    s := '[{"name":"Frodo", "age":25}, {"name":"Bobby", "age":10}]'
-    mut users := json.decode([]User, s) or {
-        eprintln('Failed to parse json')
-        return
-    }
-    for user in users {
-        println('$user.name: $user.age')
-    }
-    println('')
-    for i, mut user in users {
-        println('$i) $user.name')
-        if !user.can_register() {
-            println('Cannot register $user.name, they are too young')
-            continue
-        }
+	s := '[{"name":"Frodo", "age":25}, {"name":"Bobby", "age":10}]'
+	mut users := json.decode([]User, s) or {
+		eprintln('Failed to parse json')
+		return
+	}
+	for user in users {
+		println('\${user.name}: \${user.age}')
+	}
+	println('')
+	for i, mut user in users {
+		println('\${i}) \${user.name}')
+		if !user.can_register() {
+			println('Cannot register \${user.name}, they are too young')
+			continue
+		}
 
-        // \`user\` is declared as \`mut\` in the for loop,
-        // modifying it will modify the array
-        user.register()
-    }
+		// \`user\` is declared as \`mut\` in the for loop,
+		// modifying it will modify the array
+		user.register()
+	}
 
-    // Let's encode users again just for fun
-    println('')
-    println(json.encode(users))
+	// Let's encode users again just for fun
+	println('')
+	println(json.encode(users))
 }
 
 fn (u User) can_register() bool {
-    return u.age >= 16
+	return u.age >= 16
 }
 
 fn (mut u User) register() {
-    u.is_registered = true
+	u.is_registered = true
 }
 `,
     },
@@ -98,23 +101,23 @@ DEBUG: create new file
 DEBUG: write text to log file
 ERROR: file not writeable
 ') or {
-    // \`err\` is a special variable that contains the error
-    // in \`or {}\` blocks
-    eprintln('failed to write the file: $err')
-    return
+	// \`err\` is a special variable that contains the error
+	// in \`or {}\` blocks
+	eprintln('failed to write the file: \${err}')
+	return
 }
 
 // \`read_file\` returns a result (\`!string\`), it must be checked
 text := os.read_file('app.log') or {
-    eprintln('failed to read the file: $err')
-    return
+	eprintln('failed to read the file: \${err}')
+	return
 }
 
 lines := text.split_into_lines()
 for line in lines {
-    if line.starts_with('DEBUG:') {
-        println(line)
-    }
+	if line.starts_with('DEBUG:') {
+		println(line)
+	}
 }
 
 // DEBUG: create new file
@@ -125,46 +128,46 @@ for line in lines {
         name: "Compile-time Reflection",
         code: `
 struct User {
-    name string
-    age  int
+	name string
+	age  int
 }
 
 fn main() {
-    data := 'name=Alice\\nage=18'
-    user := decode&lt;User>(data)
-    println(user)
+	data := 'name=Alice\\nage=18'
+	user := decode[User](data)
+	println(user)
 }
 
-fn decode&lt;T>(data string) T {
-    mut result := T{}
-    // compile-time \`for\` loop
-    // T.fields gives an array of a field metadata type
-    $for field in T.fields {
-        $if field.typ is string {
-            // $(string_expr) produces an identifier
-            result.$(field.name) = get_string(data, field.name)
-        } $else $if field.typ is int {
-            result.$(field.name) = get_int(data, field.name)
-        }
-    }
-    return result
+fn decode[T](data string) T {
+	mut result := T{}
+	// compile-time \`for\` loop
+	// T.fields gives an array of a field metadata type
+	$for field in T.fields {
+		$if field.typ is string {
+			// $(string_expr) produces an identifier
+			result.$(field.name) = get_string(data, field.name)
+		} $else $if field.typ is int {
+			result.$(field.name) = get_int(data, field.name)
+		}
+	}
+	return result
 }
 
 fn get_string(data string, field_name string) string {
-    for line in data.split_into_lines() {
-        key_val := line.split('=')
-        if key_val[0] == field_name {
-            return key_val[1]
-        }
-    }
-    return ''
+	for line in data.split_into_lines() {
+		key_val := line.split('=')
+		if key_val[0] == field_name {
+			return key_val[1]
+		}
+	}
+	return ''
 }
 
 fn get_int(data string, field string) int {
-    return get_string(data, field).int()
+	return get_string(data, field).int()
 }
 
-// \`decode&lt;User>\` generates:
+// \`decode[User]\` generates:
 // fn decode_User(data string) User {
 //     mut result := User{}
 //     result.name = get_string(data, 'name')
@@ -174,10 +177,8 @@ fn get_int(data string, field string) int {
 `,
     },
 ].map((example: IExample) => {
-    example.code = example.code
-        .replaceAll("    ", "    ")
-        .replaceAll("&lt;", "<")
-        .trim()
+    example.code = example.code.trim()
+
     return example
 })
 
