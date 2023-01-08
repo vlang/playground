0613f1c073b6bde2dd0ce086fe057516ba4e701e
i582 <51853996+i582@users.noreply.github.com>
2023-01-08 15:50:27 +0400

    added ocompletion for modules

--- www/js/v_hint.js
+++ www/js/v_hint.js
@@ -9,6 +9,29 @@ const atoms = `true false nil print println exit panic error`.split(' ')
 
 const builtinTypes = `bool string i8 i16 int i64 i128 u8 u16 u32 u64 u128 rune f32 f64 isize usize voidptr any`.split(' ')
 
+const baseModules = [
+    "arrays",
+    "benchmark", "bitfield",
+    "cli", "clipboard", "compress", "context", "crypto",
+    "darwin", "datatypes", "dl", "dlmalloc",
+    "encoding", "eventbus",
+    "flag", "fontstash",
+    "gg", "gx",
+    "hash",
+    "io",
+    "js", "json",
+    "log",
+    "math", "mssql", "mysql",
+    "net",
+    "orm", "os",
+    "pg", "picoev", "picohttpparser",
+    "rand", "readline", "regex", "runtime",
+    "semver", "sokol", "sqlite", "stbi", "strconv", "strings", "sync", "szip",
+    "term", "time", "toml",
+    "v", "vweb",
+    "x",
+]
+
 function arrayContains(arr, item) {
     if (!Array.prototype.indexOf) {
         let i = arr.length;
@@ -27,6 +50,14 @@ function scriptHint(editor, keywords, getToken, options) {
     // Find the token at the cursor
     const cur = editor.getCursor();
     let token = getToken(editor, cur);
+
+    // check if token value is blank
+    if (token.string.trim() === '') {
+        const len = token.string.length;
+        const prevToken = getToken(editor, Pos(cur.line, cur.ch - len - 1));
+        context.push(prevToken);
+    }
+
     if (/\b(?:string|comment)\b/.test(token.type)) return;
     const innerMode = CodeMirror.innerMode(editor.getMode(), token.state);
     if (innerMode.mode.helperType === "json") return;
@@ -70,6 +101,20 @@ function getCompletions(token, context, options) {
     function maybeAdd(obj) {
         const str = obj.text
         if (str.lastIndexOf(start, 0) === 0 && !arrayContains(found, str)) found.push(obj);
+    }
+
+    if (context && context.length) {
+        const lastToken = context.pop();
+        if (lastToken.type === "keyword" && lastToken.string === "import") {
+            baseModules.forEach((text) => {
+                maybeAdd({
+                    text: text,
+                    displayText: text,
+                    className: 'completion-module',
+                })
+            })
+            return found;
+        }
     }
 
     keywords.forEach((text) => {
