b7081516d891a58ead214d417cc8217a815acf8b
Petr Makhnev <51853996+i582@users.noreply.github.com>
2023-04-28 23:18:19 +0400

    initial simple analytics (#1)

--- server/index_endpoint.v
+++ server/index_endpoint.v
@@ -5,6 +5,7 @@ import vweb
 
 ['/'; get]
 fn (mut app Server) index() vweb.Result {
+	app.send_analytics('/')
 	file := os.read_file('./www/public/index.html') or { panic(err) }
 	return app.html(file)
 }
