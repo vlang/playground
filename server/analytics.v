33268a74eb1c1bbf7b33daeedc4a3ef59ba07d0a
i582 <51853996+i582@users.noreply.github.com>
2023-04-29 10:07:32 +0400

    minor fixes in analytics

--- server/analytics.v
+++
@@ -1,8 +0,0 @@
-module main
-
-import net.http
-
-pub fn (mut app Server) send_analytics(url string) {
-	user_agent := app.req.header.get(http.CommonHeader.user_agent) or { '' }
-	http.post('http://localhost:8100/a', '{ "url": "${url}", "user_agent": "${user_agent}", "site_id": 0 }') or {}
-}
--- server/index_endpoint.v
+++ server/index_endpoint.v
@@ -2,10 +2,11 @@ module main
 
 import os
 import vweb
+import analytics
 
 ['/'; get]
 fn (mut app Server) index() vweb.Result {
-	app.send_analytics('/')
+	analytics.send_analytics(app.req, '/')
 	file := os.read_file('./www/public/index.html') or { panic(err) }
 	return app.html(file)
 }
