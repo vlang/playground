1447d345245136916d9ba5050e8055dc52696e78
i582 <51853996+i582@users.noreply.github.com>
2023-04-29 13:17:27 +0400

    fixed index URL for analytics

--- server/index_endpoint.v
+++ server/index_endpoint.v
@@ -6,7 +6,7 @@ import analytics
 
 ['/'; get]
 fn (mut app Server) index() vweb.Result {
-	analytics.send_analytics(app.req, '/')
+	analytics.send_analytics(app.req, 'https://play.vosca.dev/')
 	file := os.read_file('./www/public/index.html') or { panic(err) }
 	return app.html(file)
 }
